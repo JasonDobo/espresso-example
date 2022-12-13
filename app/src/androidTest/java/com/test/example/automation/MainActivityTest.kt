66dc717ad8411e9d7412a104cd77e43004ecbb7c
Jason Dobo <jasondobo@Jasons-MacBook-Pro.local>
2022-12-13 16:48:11 +0000

    Create Android project with initial espresso test

---
+++ app/src/androidTest/java/com/test/example/automation/MainActivityTest.kt
@@ -0,0 +1,35 @@
+package com.test.example.automation
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val textView = onView(
+            allOf(
+                withText(R.string.first_fragment_label),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+    }
+}
